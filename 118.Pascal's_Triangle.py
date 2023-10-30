43097e97d47aa9f94944a5c57db2a3b9e669251a
sakthiram2004 <138390991+sakthiram2004@users.noreply.github.com>
2023-10-30 21:51:56 +0530

    solved

---
+++ 118.Pascal's_Triangle.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        r=[]
+        for i in range(1,numRows+1,1):
+            t=[]
+            if i==1 or i==2:
+                for i in range(i):
+                    t.append(1)
+                r.append(t)
+            else:
+                t.append(1)
+                for j in range(i-2):
+                    s=r[i-2][j]+r[i-2][j+1]
+                    t.append(s)
+                t.append(1)
+                r.append(t)
+        return r
